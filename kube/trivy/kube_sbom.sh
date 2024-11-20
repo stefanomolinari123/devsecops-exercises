3a484482f8908b51af468db2bc30b648c66402a6
federico.bo <federico.bo@pluribus-one.it>
2024-11-20 16:12:45 +0100

    update scripts

--- kube/trivy/kube_sbom.sh
+++ kube/trivy/kube_sbom.sh
@@ -7,28 +7,39 @@ TRIVY_JAVA_DB=${TRIVY_JAVA_DB:-ghcr.io/aquasecurity/trivy-java-db,public.ecr.aws
 CLUSTER_IP=$1
 
 if [ -z "$CLUSTER_IP" ]; then
-        echo 'Missing mandatory cluster ip argument' >&2
-        exit 1
+    echo 'Missing mandatory cluster ip argument' >&2
+    exit 1
 fi
 
 #create output directory
 mkdir -p sboms
 
-#generate sbom
+echo "----- KBOM generation -----"
+#generate kbom
 docker run --rm \
-    -v "$HOME"/.kube/config-dso-user:/root/.kube/config \
-    -v ./sboms:/sboms \
+    -v "$HOME"/.kube/config-dso-user:/root/.kube/config:z \
+    -v ./sboms:/sboms:z \
     --add-host k8scp-dso:"$CLUSTER_IP" \
     aquasec/trivy k8s \
     --format cyclonedx \
     -o /sboms/mykbom.cdx.json
 
-#scan sbom for vulnerabilities
+echo "----- KBOM generation end -----"
+
+if [ ! -d "Caches" ]; then
+    tar zxf trivy_cache.tar.gz
+else
+    echo "Trivy Caches already existent"
+fi
+
+
+echo "----- KBOM vuln scan -----"
+#scan kbom for vulnerabilities
 docker run --rm \
     -it \
-    -v "$HOME"/.kube/config-dso-user:/root/.kube/config \
-    -v "$HOME"/Library/Caches:/root/.cache/ \
-    -v ./sboms:/sboms \
+    -v "$HOME"/.kube/config-dso-user:/root/.kube/config:z \
+    -v Caches:/root/.cache/:z \
+    -v ./sboms:/sboms:z \
     -e TRIVY_DB_REPOSITORY="$TRIVY_DB" \
     -e TRIVY_JAVA_DB_REPOSITORY="$TRIVY_DB_JAVA" \
     --add-host k8scp-dso:"$CLUSTER_IP" \
@@ -36,3 +47,5 @@ docker run --rm \
     -f json \
     /sboms/mykbom.cdx.json \
     -o /sboms/mykbom.cdx.vex.json
+
+echo "----- KBOM vuln scan end -----"
--- kube/trivy/kube_scan.sh
+++ kube/trivy/kube_scan.sh
@@ -7,18 +7,26 @@ TRIVY_JAVA_DB=${TRIVY_JAVA_DB:-ghcr.io/aquasecurity/trivy-java-db,public.ecr.aws
 CLUSTER_IP=$1
 
 if [ -z "$CLUSTER_IP" ]; then
-        echo 'Missing mandatory cluster ip argument' >&2
-        exit 1
+    echo 'Missing mandatory cluster ip argument' >&2
+    exit 1
 fi
 
 namespaces=dso-apps
 resources=deployment,pods
 report_type=summary # or all
 
+if [ ! -d "Caches" ]; then
+    tar zxf trivy_cache.tar.gz
+else
+    echo "Trivy Caches already existent"
+fi
+
+echo "trivy k8s scan"
+# start scanning k8s cluster @ provided ip
 docker run --rm \
     -it \
-    -v "$HOME"/.kube/config-dso-user:/root/.kube/config \
-    -v "$HOME"/Library/Caches:/root/.cache/ \
+    -v "$HOME"/.kube/config-dso-user:/root/.kube/config:z \
+    -v Caches:/root/.cache/:z \
     -e TRIVY_DB_REPOSITORY="$TRIVY_DB" \
     -e TRIVY_JAVA_DB_REPOSITORY="$TRIVY_DB_JAVA" \
     --add-host k8scp-dso:"$CLUSTER_IP" \
